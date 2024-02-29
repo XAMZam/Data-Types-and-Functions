34e96cdfc5126ec6b581d2a8357d7be331154205
ZAMZAM ALI HEDAR <Alihedarzam@gmail.com>
2024-02-29 00:16:20 -0800

    data types

---
+++ data types.drt.dart
@@ -0,0 +1,21 @@
+void main() {
+  // int data type
+  int age = 19;
+  print('Age: $age');
+
+  // double data type
+  double height = 4.2;
+  print('Height: $height');
+
+  // String data type
+  String name = 'Samiya Ibrahim';
+  print('Name: $name');
+
+  // List data type
+  List<String> fruits = ['Watermelon', 'Passion', 'Lemon'];
+  print('Fruits: $fruits');
+
+  // Map data type
+  Map<String, int> scores = {'Chemistry': 60, 'Geography': 85, 'Kiswahili': 70};
+  print('Scores: $scores');
+}
