293cb885595f62e569c1a0c6467c20ed1bb2b845
ZAMZAM ALI HEDAR <133240996+XAMZam@users.noreply.github.com>
2024-02-29 01:52:06 -0800

    Add files via upload
    data types and functios

--- data types.drt.dart
+++ data types.drt.dart
@@ -1,21 +1,41 @@
 void main() {
   // int data type
-  int age = 19;
+  int age = 30;
   print('Age: $age');
 
   // double data type
-  double height = 4.2;
-  print('Height: $height');
+  double Weight  = 70.2;
+  print('Weight: $Weight');
 
   // String data type
   String name = 'Samiya Ibrahim';
   print('Name: $name');
 
   // List data type
-  List<String> fruits = ['Watermelon', 'Passion', 'Lemon'];
-  print('Fruits: $fruits');
+  List<String> cars = ['Toyota', 'Honda', 'Ford', 'Chevrolet', 'BMW'];
+  print('Cars: $cars');
 
   // Map data type
   Map<String, int> scores = {'Chemistry': 60, 'Geography': 85, 'Kiswahili': 70};
   print('Scores: $scores');
+
+ // Using data types in a meaningful context
+  print('Hello, my name is $name. I am $age years old and $Weight kg.');
+  print('My favorite cars are ${cars.join(', ')}.');
+
+  // Accessing values in a Map
+  print('My Math score is ${scores['Math']}.');
+
+  // Adding a new key-value pair to a Map
+  scores['Kiswahili'] = 70;
+  print('My Kiswahili score is ${scores['Kiswahili']}.');
+
+  // Updating a value in a Map
+  scores['Chemistry'] = 95;
+  print('My updated Chemistry score is ${scores['Chemistry']}.');
+
+   // Removing a key-value pair from a Map
+  scores.remove('Geography');
+  print('My Geography score has been removed: $scores');
+
 }
